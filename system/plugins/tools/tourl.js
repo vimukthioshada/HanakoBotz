6bcd2609d5084487fe267a92c58db39b2288a8ba
DekuGz - Leooxzy <151708571+LeooxzyDekuu@users.noreply.github.com>
2025-01-30 23:17:16 +0700

    Update tourl.js

--- system/plugins/tools/tourl.js
+++ system/plugins/tools/tourl.js
@@ -23,49 +23,13 @@ let deku = async (m, {
 
     let q = m.quoted ? m.quoted : m;
     if (!q) throw `kirim foto trus ketik .tourl \\ reply foto trus .tourl`;
-    if (/jpg|jpeg|png/.test(q.msg.mimetype)) {
+
+    if (/audio|video|webp|sticker|image/.test(q.msg.mimetype)) {
 
         let media = await (m.quoted ? m.quoted.download() : m.download())
-
-        const {
-            ext,
-            mime
-        } = await fromBuffer(media)
-
-        let filename = './tmp/' + Date.now() + '.' + ext;
-        fs.writeFileSync(filename, media);
-        try {
-            anomakiurl = await anomakiCdn(filename)
-        } catch {
-            anomakiurl = await Uploader.catbox(media)
-        }
-        fs.unlinkSync(filename);
-        m.reply(anomakiurl)
-    } else if (/audio|video|webp|sticker/.test(q.msg.mimetype)) {
-
-        let media = await (m.quoted ? m.quoted.download() : m.download())
-
-        const {
-            ext,
-            mime
-        } = await fromBuffer(media)
-        let filename = './tmp/' + Date.now() + '.' + ext;
-        fs.writeFileSync(filename, media);
-        let formData = new FormData();
-        formData.append('file', fs.createReadStream(filename));
-        formData.append('expirationOption', 'permanent');
-        try {
-            res = await fetch('https://Nauval.mycdn.biz.id/upload', {
-                method: 'POST',
-                body: formData
-            })
-        } catch {
-            res = await Uploader.catbox(media)
-        }
-        let json = await res.json();
-
-        fs.unlinkSync(filename);
-        m.reply(json.fileUrl)
+        const catbox = await Uploader.catbox(media)
+        const res = await api(media).catch(e => catbox)
+        m.reply(res)
     } else {
         m.reply(`kirim foto trus ketik .tourl \\ reply foto trus .tourl`);
     }
@@ -82,35 +46,17 @@ deku.loading = true
 
 module.exports = deku
 
-const createFormData = (content, fieldName, ext) => {
-    const {
-        mime
-    } = fromBuffer(content) || {};
-    const formData = new FormData();
-    formData.append(fieldName, content, `${randomBytes}.${ext}`);
-    return formData;
-};
-
-async function anomakiCdn(path) {
-    if (!path) throw new Error("Masukkan Path Yang Benar.");
-
-    try {
-        const d = new FormData();
-        d.append("files", fs.createReadStream(path));
-
-        const headers = {
-            headers: {
-                ...d.getHeaders()
-            }
-        };
-
-        const {
-            data: p
-        } = await axios.post("https://cdn.anomaki.web.id/api/upload", d, headers);
-        const valid = "https://cdn.anomaki.web.id" + p[0].url;
-
-        return valid;
-    } catch (error) {
-        throw new Error(error.message);
-    }
+const api = async (buffer) => {
+    let {
+        ext
+    } = await fromBuffer(buffer);
+    let bodyForm = new FormData();
+    bodyForm.append("file", buffer, "file." + ext);
+    let res = await fetch("https://file.btch.rf.gd/api/upload.php", {
+        method: "post",
+        body: bodyForm,
+    });
+    let data = await res.json();
+    let resultUrl = data.result ? data.result.url : '';
+    return resultUrl;
 }
