1327d6328d17dec361dba5119e3f9e84466fbf87
DekuGz - Leooxzy <151708571+LeooxzyDekuu@users.noreply.github.com>
2025-02-15 19:49:08 +0700

    Update autoanimeup.js

--- system/plugins/owner/autoanimeup.js
+++ system/plugins/owner/autoanimeup.js
@@ -1,25 +1,26 @@
+async function run(m, {
+    sock,
+    text
+}) {
+    const args = m.args
+    if (!text) throw 'mau pilih yang mana on? or off?'
+    if (args[0] === "off") {
+        db.list().settings.otakudesu = false
+        m.reply('oke udah di offin')
+    } else if (args[0] === "on") {
+        db.list().settings.otakudesu = true
+        m.reply('oke udah di om')
+    }
+}
+
 module.exports = {
-command: "autoanimeup",
-alias: [ ],
-category: ["owner"],
+    command: "autoanimeup",
+    alias: [],
+    category: ["owner"],
     settings: {
-    owner: true,
-  },
-description: "autoanimeup",
-loading: true,
-async run(m, { sock, text }) {
-
-const args = m.args
-
-if (!text) throw 'mau pilih yang mana on? or off?'
-
-if (args[0] === "off") {
- db.list().settings.animeup = false
- m.reply('oke udah di offin')
-} else if (args[0] === "on") {
- db.list().settings.animeup = true
- m.reply('oke udah di offin')
-}
-
-}
+        owner: true,
+    },
+    description: "autoanimeup",
+    loading: true,
+    run
 }
