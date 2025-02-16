5a1635bfaad43c34fb881328caa8f95cdf4cbda6
DekuGz - Leooxzy <151708571+LeooxzyDekuu@users.noreply.github.com>
2025-02-16 20:41:08 +0700

    Update ytmp3cc.js

--- scrapers/src/ytmp3cc.js
+++ scrapers/src/ytmp3cc.js
@@ -1,14 +1,14 @@
-// source : https://whatsapp.com/channel/0029VaAMjXT4yltWm1NBJV3J
 const axios = require('axios')
 
 /* ndbotz */
-function getTokens(nyxz, ndbz) {
-    for (var t = 0; t < atob(nyxz[0]).split(nyxz.f[6]).length; t++) 
-        ndbz += (0 < nyxz.f[5] ? nyxz[1].split('').reverse().join('') : nyxz[1])[atob(nyxz[0]).split(nyxz.f[6])[t] - nyxz.f[4]];
-    return 1 == nyxz.f[2] ? ndbz = ndbz.toLowerCase() : 2 == nyxz.f[2] && (ndbz = ndbz.toUpperCase()),
-    0 < nyxz.f[1].length ? nyxz.f[1] : 0 < nyxz.f[3] ? ndbz.substring(0, nyxz.f[3] + 1) : ndbz
+function tokenizer(nyxz, ndbz) {
+    if (eval(atob(nyxz.t[0])) == nyxz.t[1]) {
+        for (var c = 0; c < atob(nyxz[0]).split(nyxz.f[5]).length; c++) ndbz += (0 < nyxz.f[4] ? nyxz[1].split("").reverse().join("") : nyxz[1])[atob(nyxz[0]).split(nyxz.f[5])[c] - nyxz.f[3]];
+        return 1 == nyxz.f[1] ? ndbz = ndbz.toLowerCase() : 2 == nyxz.f[1] && (ndbz = ndbz.toUpperCase()), 0 < nyxz.f[0].length ? nyxz.f[0] : 0 < nyxz.f[2] ? ndbz.substring(0, nyxz.f[2] + 1) : ndbz
+    }
 }
 /* ndbotz */
+
 const headers = {
     'Accept': '*/*',
     'Accept-Language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
@@ -43,7 +43,7 @@ async function ytdl(url, type = 'mp3') {
     const webb = await axios.get('https://ytmp3.cc/Vluk/', { headers });
 /* ndbotz */
     const tokenJson = JSON.parse(atob(webb.data?.match(/atob\('(.*?)'\)/)?.[1]).match(/var gC = ({[\s\S]*?});/)?.[1]); 
-    const token = btoa(tokenJson[2] +'-'+ getTokens(tokenJson, tokenJson.f[7]));
+    const token = btoa(tokenJson[2] + "-" + tokenizer(tokenJson, tokenJson.f[6]))
 
     const init = await axios.get(`https://d.ecoe.cc/api/v1/init?k=${token}&_=${Math.random()}`, { headers }).then(x => x.data);
     const convert = await axios.get(`${init.convertURL}&v=https://www.youtube.com/watch?v=${vidId}&f=${type}&_=${Math.random()}`, { headers }).then(x => x.data);
@@ -63,7 +63,7 @@ async function ytdl(url, type = 'mp3') {
         res = await axios.get(convert.progressURL, { headers }).then(x => x.data);
         await new Promise(rv => setTimeout(rv, 1000));
         retry++
-      } while (res.progress !== 3);
+      } while (res.progress < 3);
       return {
         type,
         title: res.title,
@@ -71,7 +71,8 @@ async function ytdl(url, type = 'mp3') {
       };
     };
   } catch(e) {
-    throw new Error(`Eror bang, nanti aja download nya, nih log eror nya: ${e.message} 😌`);
+    let err = new Error(`Eror bang, nanti aja download nya, nih log eror nya: ${e.message}`); err.error = e;
+    throw err
   }
 }
 
