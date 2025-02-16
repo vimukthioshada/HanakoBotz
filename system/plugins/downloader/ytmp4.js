a73612a84451d1b250e39a3604463fb503079af8
DekuGz - Leooxzy <151708571+LeooxzyDekuu@users.noreply.github.com>
2025-02-16 20:35:12 +0700

    Update ytmp4.js

--- system/plugins/downloader/ytmp4.js
+++ system/plugins/downloader/ytmp4.js
@@ -1,96 +1,112 @@
-const axios = require("axios");
 const yts = require('yt-search')
 const ytdl = require('ytdl-core')
-const fs = require('fs')
 
-let deku = async (m, {
-    sock,
-    Func,
-    Scraper,
-    Uploader,
-    store,
-    text,
-    config
-}) => {
-
-    if (!text.includes('youtu')) throw `Link Contoh ${m.prefix + m.command} <link>`;
-    const videoId = await ytdl.getURLVideoID(text)
-    if (!videoId) return m.reply(Func.Styles('Maaf VideoId nya ga valid'))
-    let result = await yts({
-        videoId: videoId,
-        hl: 'id',
-        gl: 'ID'
-    })
-
-    let hah = result.url;
-    let deku = Func.Styles(`⏤͟͟͞͞╳── *[ ytv - download ]* ── .々─ᯤ\n`)
-    deku += `│    =〆 ᴛɪᴛʟᴇ: ${result.title}\n`
-    deku += `│    =〆 ɪᴅ: ${result.videoId}\n`
-    deku += `│    =〆 ᴅᴜʀᴀsɪ: ${result.timestamp}\n`
-    deku += `│    =〆 ᴀɢᴏ: ${result.ago}\n`
-    deku += `│    =〆 ᴅᴇsᴄʀɪᴘᴛɪᴏɴ: ${result.description}\n`
-    deku += `│    =〆 ᴜʀʟ: ${result.url}\n`
-    deku += `⏤͟͟͞͞╳────────── .✦`
-
-    await sock.sendMessage(m.cht, {
-        text: deku,
-        contextInfo: {
-            forwardingScore: 999,
-            isForwarded: true,
-            externalAdReply: {
-                title: result.title,
-                mediaType: 1,
-                previewType: 1,
-                body: `Durasi : ${result.timestamp} / View : ${result.views}`,
-                thumbnailUrl: result.image,
-                renderLargerThumbnail: true,
-                mediaUrl: result.url,
-                sourceUrl: result.url
-            }
+class Command {
+    constructor() {
+        this.command = "ytmp4"
+        this.alias = [
+            "ytv",
+            "yt-video"
+        ]
+        this.category = [
+            "downloader"
+        ]
+        this.settings = {
+            limit: true
         }
-    }, {
-        quoted: m
-    });
+        this.description = "Mendownload YouTube Video"
+        this.loading = true
+    }
+    run = async (m, {
+        sock,
+        client,
+        conn,
+        DekuGanz,
+        Func,
+        Scraper,
+        text,
+        config
+    }) => {
+        if (!text.includes('youtu')) throw `Link Contoh ${m.prefix + m.command} <link>`;
+        const videoId = await ytdl.getURLVideoID(text)
+        if (!videoId) return m.reply(Func.Styles('Maaf VideoId nya ga valid'))
+        let result = await yts({
+            videoId: videoId,
+            hl: 'id',
+            gl: 'ID'
+        })
 
-    const {
-        downloadUrl
-    } = await Scraper.ddownr.download(text, '720')
+        let hah = result.url;
+        let deku = Func.Styles(`⏤͟͟͞͞╳── *[ ytv - download ]* ── .々─ᯤ\n`)
+        deku += `│    =〆 ᴛɪᴛʟᴇ: ${result.title}\n`
+        deku += `│    =〆 ɪᴅ: ${result.videoId}\n`
+        deku += `│    =〆 ᴅᴜʀᴀsɪ: ${result.timestamp}\n`
+        deku += `│    =〆 ᴀɢᴏ: ${result.ago}\n`
+        deku += `│    =〆 ᴅᴇsᴄʀɪᴘᴛɪᴏɴ: ${result.description}\n`
+        deku += `│    =〆 ᴜʀʟ: ${result.url}\n`
+        deku += `⏤͟͟͞͞╳────────── .✦`
 
-    let capt = ` =〆 ᴛɪᴛʟᴇ: ${result.title}\n`
-    capt += ` =〆 ɪᴅ: ${result.videoId}\n`
-    capt += ` =〆 ᴅᴜʀᴀsɪ: ${result.timestamp}\n`
-    capt += ` =〆 ᴀɢᴏ: ${result.ago}\n`
-    capt += ` =〆 ᴜʀʟ: ${result.url}`
-
-    try {
-        sock.sendMessage(m.cht, {
-            video: {
-                url: downloadUrl
-            },
-            mimetype: 'video/mp4',
-            fileName: result.title + '.mp4',
-            caption: capt
+        await sock.sendMessage(m.cht, {
+            text: deku,
+            contextInfo: {
+                forwardingScore: 999,
+                isForwarded: true,
+                externalAdReply: {
+                    title: result.title,
+                    mediaType: 1,
+                    previewType: 1,
+                    body: `Durasi : ${result.timestamp} / View : ${result.views}`,
+                    thumbnailUrl: result.image,
+                    renderLargerThumbnail: true,
+                    mediaUrl: result.url,
+                    sourceUrl: result.url
+                }
+            }
         }, {
             quoted: m
-        })
-    } catch (err) {
-        m.reply('error' + err)
+        });
+
+        let capt = ` =〆 ᴛɪᴛʟᴇ: ${result.title}\n`
+        capt += ` =〆 ɪᴅ: ${result.videoId}\n`
+        capt += ` =〆 ᴅᴜʀᴀsɪ: ${result.timestamp}\n`
+        capt += ` =〆 ᴀɢᴏ: ${result.ago}\n`
+        capt += ` =〆 ᴜʀʟ: ${result.url}`
+
+        try {
+            const {
+                link
+            } = await Scraper.ytmp3cc(text, 'mp4')
+            sock.sendMessage(m.cht, {
+                video: {
+                    url: link
+                },
+                mimetype: 'video/mp4',
+                fileName: result.title + '.mp4',
+                caption: capt
+            }, {
+                quoted: m
+            })
+        } catch (err) {
+            try {
+                const {
+                    downloadUrl
+                } = await Scraper.ddownr.download(text, '720')
+                sock.sendMessage(m.cht, {
+                    video: {
+                        url: downloadUrl
+                    },
+                    mimetype: 'video/mp4',
+                    fileName: result.title + '.mp4',
+                    caption: capt
+                }, {
+                    quoted: m
+                })
+            } catch (err) {
+                m.reply('error' + err)
+            }
+        }
+        m.react('✅')
     }
-    m.react('✅')
 }
 
-deku.command = "ytmp4"
-deku.alias = [
-    "ytv",
-    "yt-video"
-]
-deku.category = [
-    "downloader"
-]
-deku.settings = {
-    limit: true
-}
-deku.description = "Mendownload YouTube Video"
-deku.loading = true
-
-module.exports = deku
+module.exports = new Command();
