a12cd80783814aa3dc61b8d28c7cc974d3d68ef7
DekuGz - Leooxzy <151708571+LeooxzyDekuu@users.noreply.github.com>
2025-02-16 20:48:58 +0700

    Update ytmp3.js

--- system/plugins/downloader/ytmp3.js
+++ system/plugins/downloader/ytmp3.js
@@ -2,84 +2,103 @@ const yts = require("yt-search");
 const ytdl = require('ytdl-core')
 const axios = require('axios')
 
-let deku = async (m, {
-    sock,
-    Func,
-    Scraper,
-    Uploader,
-    store,
-    text,
-    config
-}) => {
-
-    if (!text.includes('youtu')) throw `Link Contoh ${m.prefix + m.command} <link>`;
-    const videoId = await ytdl.getURLVideoID(text)
-    if (!videoId) return m.reply(Func.Styles('Maaf VideoId nya ga valid'))
-    let result = await yts({
-        videoId: videoId,
-        hl: 'id',
-        gl: 'ID'
-    })
-
-    let hah = result.url;
-    let deku = Func.Styles(`⏤͟͟͞͞╳── *[ yta - download ]* ── .々─ᯤ\n`)
-    deku += `│    =〆 ᴛɪᴛʟᴇ: ${result.title}\n`
-    deku += `│    =〆 ɪᴅ: ${result.videoId}\n`
-    deku += `│    =〆 ᴅᴜʀᴀsɪ: ${result.timestamp}\n`
-    deku += `│    =〆 ᴀɢᴏ: ${result.ago}\n`
-    deku += `│    =〆 ᴅᴇsᴄʀɪᴘᴛɪᴏɴ: ${result.description}\n`
-    deku += `│    =〆 ᴜʀʟ: ${result.url}\n`
-    deku += `⏤͟͟͞͞╳────────── .✦`
-
-    await sock.sendMessage(m.cht, {
-        text: deku,
-        contextInfo: {
-            forwardingScore: 999,
-            isForwarded: true,
-            externalAdReply: {
-                title: result.title,
-                mediaType: 1,
-                previewType: 1,
-                body: `Durasi : ${result.timestamp} / View : ${result.views}`,
-                thumbnailUrl: result.image,
-                renderLargerThumbnail: true,
-                mediaUrl: result.url,
-                sourceUrl: result.url
-            }
+class Command {
+    constructor() {
+        this.command = "ytmp3"
+        this.alias = [
+            "yta",
+            "yt-audio"
+        ]
+        this.category = [
+            "downloader"
+        ]
+        this.settings = {
+            limit: true
         }
-    }, {
-        quoted: m
-    })
-    try {
-        const {
-            downloadUrl
-        } = await Scraper.ddownr.download(text, 'mp3')
+        this.description = "Mendownload YouTube Audio"
+        this.loading = true
+    }
+    run = async (m, {
+        sock,
+        client,
+        conn,
+        DekuGanz,
+        Func,
+        Scraper,
+        text,
+        config
+    }) => {
+
+        if (!text.includes('youtu')) throw `Link Contoh ${m.prefix + m.command} <link>`;
+        const videoId = await ytdl.getURLVideoID(text)
+        if (!videoId) return m.reply(Func.Styles('Maaf VideoId nya ga valid'))
+        let result = await yts({
+            videoId: videoId,
+            hl: 'id',
+            gl: 'ID'
+        })
+
+        let hah = result.url;
+        let deku = Func.Styles(`⏤͟͟͞͞╳── *[ yta - download ]* ── .々─ᯤ\n`)
+        deku += `│    =〆 ᴛɪᴛʟᴇ: ${result.title}\n`
+        deku += `│    =〆 ɪᴅ: ${result.videoId}\n`
+        deku += `│    =〆 ᴅᴜʀᴀsɪ: ${result.timestamp}\n`
+        deku += `│    =〆 ᴀɢᴏ: ${result.ago}\n`
+        deku += `│    =〆 ᴅᴇsᴄʀɪᴘᴛɪᴏɴ: ${result.description}\n`
+        deku += `│    =〆 ᴜʀʟ: ${result.url}\n`
+        deku += `⏤͟͟͞͞╳────────── .✦`
+
         await sock.sendMessage(m.cht, {
-            audio: {
-                url: downloadUrl
-            },
-            mimetype: 'audio/mpeg'
+            text: deku,
+            contextInfo: {
+                forwardingScore: 999,
+                isForwarded: true,
+                externalAdReply: {
+                    title: result.title,
+                    mediaType: 1,
+                    previewType: 1,
+                    body: `Durasi : ${result.timestamp} / View : ${result.views}`,
+                    thumbnailUrl: result.image,
+                    renderLargerThumbnail: true,
+                    mediaUrl: result.url,
+                    sourceUrl: result.url
+                }
+            }
         }, {
             quoted: m
         })
-    } catch (err) {
-        m.reply('error' + err)
-    };
-    m.react('✅')
+
+        try {
+            const {
+                link
+            } = await Scraper.ytmp3cc(text, 'mp3')
+            await sock.sendMessage(m.cht, {
+                audio: {
+                    url: link
+                },
+                mimetype: 'audio/mpeg'
+            }, {
+                quoted: m
+            })
+        } catch (err) {
+            try {
+                const {
+                    downloadUrl
+                } = await Scraper.ddownr.download(text, 'mp3')
+                await sock.sendMessage(m.cht, {
+                    audio: {
+                        url: downloadUrl
+                    },
+                    mimetype: 'audio/mpeg'
+                }, {
+                    quoted: m
+                })
+            } catch (err) {
+                m.reply('error' + err)
+            }
+        };
+        m.react('✅')
+    }
 }
 
-deku.command = "ytmp3"
-deku.alias = [
-    "yta",
-    "yt-audio"
-]
-deku.category = [
-    "downloader"
-]
-deku.settings = {
-    limit: true
-}
-deku.description = "Mendownload YouTube Audio"
-deku.loading = true
-
-module.exports = deku
+module.exports = new Command();
