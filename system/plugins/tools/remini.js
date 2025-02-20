9aaee9430b44f8b01504240fbacd05cfd221640c
DekuGz - Leooxzy <151708571+LeooxzyDekuu@users.noreply.github.com>
2025-02-20 09:48:35 +0700

    Update remini.js

--- system/plugins/tools/remini.js
+++ system/plugins/tools/remini.js
@@ -1,22 +1,70 @@
-module.exports = {
-  command: "remini",
-  alias: ["hdr", "hd"],
-  category: ["tools"],
-  settings: {
-    limit: true,
-  },
-  description: "Tingkatkan kualitas Photo mu !",
-  loading: true,
-  async run(m, { sock, Scraper, Func }) {
-    let q = m.quoted ? m.quoted : m;
-    if (!/image/.test(q.msg.mimetype) || !q.isMedia)
-      throw `> Reply/Kirim photo yang mau di jernihkan`;
-    let buffer = await q.download();
-    let data = await Scraper.remini(buffer);
-    let size = Func.formatSize(data.length);
-    m.reply({
-      image: data,
-      caption: `*– 乂 Remini - Image*\n> *- Ukuran photo :* ${size}`,
-    });
-  },
-};
+class Command {
+    constructor() {
+        this.command = "remini"
+        this.alias = [
+            "hdr", "hd"
+        ]
+        this.category = [
+            "tools"
+        ]
+        this.settings = {
+            limit: true
+        }
+        this.description = "menjernihkan Foto"
+        this.loading = true
+    }
+    run = async (m, {
+        sock,
+        client,
+        conn,
+        DekuGanz,
+        Func,
+        Uploader,
+        Scraper,
+        text,
+        config
+    }) => {
+
+        /**
+         * Contoh penggunaaan
+         */
+        let quoted = m.quoted ? m.quoted : m;
+        if (!/image/.test(quoted.msg.mimetype) || !quoted.isMedia)
+            throw `> Reply/Kirim photo yang mau di jernihkan`;
+        try {
+            const media = await quoted.download()
+            const IMAGE = await Uploader.tmpfiles(media);
+            const SETTINGS = {
+                face_enhance: {
+                    model: "remini"
+                },
+                background_enhance: {
+                    model: "rhino-tensorrt"
+                },
+                bokeh: {
+                    aperture_radius: "0",
+                    highlights: "0.20",
+                    vivid: "0.75",
+                    group_picture: "true",
+                    rescale_kernel_for_small_images: "true",
+                    apply_front_bokeh: "false"
+                },
+                jpeg_quality: 90
+            }
+            const result = await Scraper.reminiweb(IMAGE, SETTINGS); // Buffer atau Foto
+            const Ukuran = await Func.getSize(result.no_wm)
+            sock.sendMessage(m.cht, {
+                image: {
+                    url: result.no_wm
+                },
+                caption: Func.Styles(`📷 Photo Remini\n> • 📁 *Size: ${Ukuran}*`)
+            }, {
+                quoted: m
+            })
+        } catch (e) {
+            m.reply('maaf terjadi error: ' + e)
+        }
+    }
+}
+
+module.exports = new Command();
